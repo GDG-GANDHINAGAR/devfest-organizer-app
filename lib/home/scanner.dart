74a7a0211a6af5a17c7adc6859038c83f857e218
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-10-11 16:20:03 +0530

    Enhanced UI

--- lib/home/scanner.dart
+++ lib/home/scanner.dart
@@ -7,24 +7,42 @@ class ScannerPage extends StatefulWidget {
 }
 
 class _ScannerPageState extends State<ScannerPage> {
-  String message = "Start Scanning";
+  String message = "Start Scanning ;)";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scanner"),
+        title: Text("QR Code Scanner"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("$message"),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
             RaisedButton(
+              highlightColor: Colors.green,
+              highlightElevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 6,
+                ),
+                child: Text(
+                  "Scan Now",
+                  textScaleFactor: 1.4,
+                  style: TextStyle(),
+                ),
+              ),
               onPressed: () async {
-                message = await QRCodeReader().scan();
+                message = await QRCodeReader().scan() ?? "Start Scanning ;)";
                 setState(() {});
               },
-              child: Text("Scan"),
             ),
           ],
         ),
