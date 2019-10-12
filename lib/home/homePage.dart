0cf9291af203fb039d7ba7bc3f399c0bf9717e76
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-10-12 22:29:38 +0530

    Connect qr code scanner to firebase

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -31,162 +31,193 @@ class _HomePageState extends State<HomePage> {
               child: Text("Error Occured: ${snapshot.error}"),
             );
           }
-          return Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Card(
-                      child: ListTile(
-                        // trailing: Icon(Icons.edit),
-                        leading: Icon(
-                          Icons.person,
-                          size: 40,
+          return SingleChildScrollView(
+            child: Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Card(
+                        child: ListTile(
+                          // trailing: Icon(Icons.edit),
+                          leading: Icon(
+                            Icons.person,
+                            size: 40,
+                          ),
+                          title: Text("Edit Speaker Details"),
+                          subtitle:
+                              Text("Edit Speaker visibility and talk timings"),
+                          isThreeLine: true,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return SpeakerPage();
+                                },
+                              ),
+                            );
+                          },
                         ),
-                        title: Text("Edit Speaker Details"),
-                        subtitle:
-                            Text("Edit Speaker visibility and talk timings"),
-                        isThreeLine: true,
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return SpeakerPage();
-                              },
-                            ),
-                          );
-                        },
                       ),
                     ),
-                  ),
-                  // true // add condition here
-                  snapshot.data["meta"]["feedback_active"] ?? false
-                      ? Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Card(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.feedback,
-                                size: 40,
-                              ),
-                              title: Text("Disable Feedback"),
-                              subtitle: Text(
-                                  "Toggle Feedback form visibility in app"),
-                              isThreeLine: true,
-                              onTap: () {
-                                return showDialog(
-                                  barrierDismissible: true,
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      content: const Text(
-                                          "The feedback button would disappear from the home screen and users would not be able to access the feedback form"),
-                                      title: Text("Disable Feedback"),
-                                      actions: <Widget>[
-                                        FlatButton(
-                                          child: Text("Turn Off"),
-                                          onPressed: () async {
-                                            await Firestore.instance
-                                                .collection("homepage")
-                                                .document("dummy_data")
-                                                .updateData({
-                                              "meta.feedback_active": false,
-                                            }).then((onValue) {
-                                              print("Feedback turned off");
+                    // true // add condition here
+                    snapshot.data["meta"]["feedback_active"] ?? false
+                        ? Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Card(
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.feedback,
+                                  size: 40,
+                                ),
+                                title: Text("Disable Feedback"),
+                                subtitle: Text(
+                                    "Toggle Feedback form visibility in app"),
+                                isThreeLine: true,
+                                onTap: () {
+                                  return showDialog(
+                                    barrierDismissible: true,
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        content: const Text(
+                                            "The feedback button would disappear from the home screen and users would not be able to access the feedback form"),
+                                        title: Text("Disable Feedback"),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                            child: Text("Turn Off"),
+                                            onPressed: () async {
+                                              await Firestore.instance
+                                                  .collection("homepage")
+                                                  .document("dummy_data")
+                                                  .updateData({
+                                                "meta.feedback_active": false,
+                                              }).then((onValue) {
+                                                print("Feedback turned off");
+                                                Navigator.of(context).pop();
+                                              });
+                                            },
+                                          ),
+                                          FlatButton(
+                                            child: Text("Cancel"),
+                                            onPressed: () {
                                               Navigator.of(context).pop();
-                                            });
-                                          },
-                                        ),
-                                        FlatButton(
-                                          child: Text("Cancel"),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          )
+                        : Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Card(
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.feedback,
+                                  size: 40,
+                                ),
+                                title: Text("Enable Feedback"),
+                                subtitle: Text(
+                                    "Toggle Feedback form visibility in app"),
+                                isThreeLine: true,
+                                onTap: () {
+                                  return showDialog(
+                                    barrierDismissible: true,
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        content: const Text(
+                                            "A new button would appear on the home screen enabling the users to provide their feedback adter the fest."),
+                                        title: Text("Enable Feedback"),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                            child: Text("Turn On"),
+                                            onPressed: () async {
+                                              await Firestore.instance
+                                                  .collection("homepage")
+                                                  .document("dummy_data")
+                                                  .updateData({
+                                                "meta.feedback_active": true,
+                                              }).then((onValue) {
+                                                print("Feedback turned on");
+                                                Navigator.of(context).pop();
+                                              });
+                                            },
+                                          ),
+                                          FlatButton(
+                                            child: Text("Cancel"),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
                             ),
                           ),
-                        )
-                      : Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Card(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.feedback,
-                                size: 40,
-                              ),
-                              title: Text("Enable Feedback"),
-                              subtitle: Text(
-                                  "Toggle Feedback form visibility in app"),
-                              isThreeLine: true,
-                              onTap: () {
-                                return showDialog(
-                                  barrierDismissible: true,
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      content: const Text(
-                                          "A new button would appear on the home screen enabling the users to provide their feedback adter the fest."),
-                                      title: Text("Enable Feedback"),
-                                      actions: <Widget>[
-                                        FlatButton(
-                                          child: Text("Turn On"),
-                                          onPressed: () async {
-                                            await Firestore.instance
-                                                .collection("homepage")
-                                                .document("dummy_data")
-                                                .updateData({
-                                              "meta.feedback_active": true,
-                                            }).then((onValue) {
-                                              print("Feedback turned on");
-                                              Navigator.of(context).pop();
-                                            });
-                                          },
-                                        ),
-                                        FlatButton(
-                                          child: Text("Cancel"),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.camera_alt,
+                            size: 40,
                           ),
+                          title: Text("QR Code Scanner (Schwags)"),
+                          subtitle: Text(
+                              "Scan QR codes to verify if attendees have collected schwags"),
+                          isThreeLine: true,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return ScannerPage(
+                                    isCheckingFood: false,
+                                  );
+                                },
+                              ),
+                            );
+                          },
                         ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(
-                          Icons.camera_alt,
-                          size: 40,
-                        ),
-                        title: Text("QR Code Scanner"),
-                        subtitle: Text("Scan QR codes and verify tickets"),
-                        isThreeLine: true,
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return ScannerPage();
-                              },
-                            ),
-                          );
-                        },
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.camera_alt,
+                            size: 40,
+                          ),
+                          title: Text("QR Code Scanner (Food)"),
+                          subtitle: Text(
+                              "Scan QR codes to verify if attendees have taken food"),
+                          isThreeLine: true,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return ScannerPage(
+                                    isCheckingFood: true,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
--- lib/home/scanner.dart
+++ lib/home/scanner.dart
@@ -1,48 +1,157 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:qrcode_reader/qrcode_reader.dart';
 
 class ScannerPage extends StatefulWidget {
+  final bool isCheckingFood;
+  ScannerPage({@required this.isCheckingFood});
   @override
   _ScannerPageState createState() => _ScannerPageState();
 }
 
 class _ScannerPageState extends State<ScannerPage> {
   String message = "Start Scanning ;)";
+  String commodity;
+  DocumentSnapshot snapshot;
+  bool scanned = true;
+  bool exists = true;
+  String id;
+
   @override
   Widget build(BuildContext context) {
+    commodity = widget.isCheckingFood ? "food" : "schwags";
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("QR Code Scanner"),
+        title: Text("QR Code Scanner (${commodity.toUpperCase()})"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("$message"),
+            Text(
+              "$message",
+              textAlign: TextAlign.center,
+            ),
             SizedBox(
               height: MediaQuery.of(context).size.height / 6,
             ),
-            RaisedButton(
-              highlightColor: Colors.green,
-              highlightElevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 6,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  highlightColor: Colors.green,
+                  highlightElevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 6,
+                    ),
+                    child: Text(
+                      "Scan Now",
+                      textScaleFactor: 1.4,
+                      style: TextStyle(),
+                    ),
+                  ),
+                  onPressed: () async {
+                    scanned = true;
+                    exists = true;
+                    setState(() {
+                      message = "Start Scanning ;)";
+                    });
+
+                    message = await QRCodeReader().scan();
+                    scanned = message == null ? false : true;
+                    id = message;
+
+                    //do stuff only if something is scanned
+                    //else just show welcome string
+                    if (scanned) {
+                      snapshot = await Firestore.instance
+                          .collection("attendees")
+                          .document("data")
+                          .get();
+                      exists =
+                          snapshot.data["data"][message] == null ? false : true;
+
+                      //check if data exists or not on firebase
+                      //proceed towards further checks onyly if
+                      //id exists on firebase
+                      if (exists) {
+                        //check if schwags have already been collected
+                        //when data is NOT been NULL
+                        if (snapshot.data["data"][message][commodity] == true) {
+                          message =
+                              "${commodity.toUpperCase()} ALREADY COLLECTED!!";
+                        }
+                        //if schwags have not been collected, only then
+                        //set the schwags to TRUE
+                        else {
+                          await Firestore.instance
+                              .collection("attendees")
+                              .document("data")
+                              .updateData({
+                            "data.$message.$commodity": true,
+                          }).then((onValue) {
+                            print("Data has been set");
+                            message = "ID $id \nMarked as Collected";
+                          });
+                        }
+                      } else {
+                        message = "Scanned $id\n\nNO DATA FOUND";
+                      }
+                    } else {
+                      message = "Start Scanning ;)";
+                    }
+
+                    setState(() {});
+                  },
                 ),
-                child: Text(
-                  "Scan Now",
-                  textScaleFactor: 1.4,
-                  style: TextStyle(),
+                RaisedButton(
+                  highlightColor: Colors.green,
+                  highlightElevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 6,
+                    ),
+                    child: Text(
+                      "Undo",
+                      textScaleFactor: 1.4,
+                      style: TextStyle(),
+                    ),
+                  ),
+                  onPressed: () async {
+                    snapshot = await Firestore.instance
+                        .collection("attendees")
+                        .document("data")
+                        .get();
+                    if (scanned == true &&
+                        exists == true &&
+                        snapshot.data["data"][id][commodity] == true) {
+                      await Firestore.instance
+                          .collection("attendees")
+                          .document("data")
+                          .updateData({
+                        "data.$id.$commodity": false,
+                      }).then((onValue) {
+                        print("Data has been unset");
+                        message = "ID $id \nMarked as NOT Collected";
+                      });
+                    } else {
+                      print("Nothing to UNDO");
+                      message = "Nothing to Undo";
+                    }
+                    setState(() {});
+                  },
                 ),
-              ),
-              onPressed: () async {
-                message = await QRCodeReader().scan() ?? "Start Scanning ;)";
-                setState(() {});
-              },
+              ],
             ),
           ],
         ),
